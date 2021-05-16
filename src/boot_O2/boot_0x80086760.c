42354bf3672de776530124095c099480f23a71a7
angie <angheloalf95@gmail.com>
2021-05-15 23:32:12 -0400

    new file

---
+++ src/boot_O2/boot_0x80086760.c
@@ -0,0 +1,68 @@
+#include <ultra64.h>
+#include <global.h>
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086760.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086794.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_800867B4.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_800867D4.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_800867F4.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086814.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086834.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086880.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_800869A4.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086AF0.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086B30.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086C18.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086C48.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086C70.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086C7C.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086C88.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086C98.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086CA8.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086CB4.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086CC0.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086CD0.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086CE0.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086CEC.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086CF8.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086D08.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086D18.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086D24.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086D30.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086D40.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086D50.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086D6C.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086D8C.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/boot/boot_0x80086760/func_80086DAC.asm")
