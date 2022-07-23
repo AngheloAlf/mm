b3b99e2d5b8fb9fc459939a1a529790be8f1d181
Angie <angheloalf95@gmail.com>
2022-07-23 16:25:10 -0400

    PRINTER

---
+++ include/az.h
@@ -0,0 +1,32 @@
+#ifndef AZ_H
+#define AZ_H
+
+#define AZ_PRINTER __az_printer
+
+#define AZ_OPEN_PRINTER_OVERLAY() \
+    { \
+        GfxPrint AZ_PRINTER; \
+        Gfx* __az_gfxRef; \
+        Gfx* __az_gfx; \
+         \
+        GfxPrint_Init(&AZ_PRINTER); \
+         \
+        __az_gfxRef = POLY_OPA_DISP; \
+        __az_gfx = Graph_GfxPlusOne(__az_gfxRef); \
+        gSPDisplayList(OVERLAY_DISP++, __az_gfx); \
+         \
+        GfxPrint_Open(&AZ_PRINTER, __az_gfx)
+
+
+#define AZ_CLOSE_PRINTER_OVERLAY() \
+        __az_gfx = GfxPrint_Close(&AZ_PRINTER); \
+         \
+        gSPEndDisplayList(__az_gfx++); \
+        Graph_BranchDlist(__az_gfxRef, __az_gfx); \
+        POLY_OPA_DISP = __az_gfx; \
+         \
+        GfxPrint_Destroy(&AZ_PRINTER); \
+    } \
+    (void)0
+
+#endif
